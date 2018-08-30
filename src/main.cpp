8c6ae364e161c71dbf1812ce30ce3fc889000fc8
gamma-delta <joshua.j.park@gmail.com>
2018-08-29 20:27:33 -0700

    Got the player moving!

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "classes/Object.h"
 #include "classes/Player.h"
 
-void introduce_object(Object o);
+void introduce_object(Object* o);
 std::vector<Object*> getObjectsAtXY(int x, int y);
 void refresh_map();
 void display_map();
@@ -16,19 +16,19 @@ extern const int MAP_HEIGHT = 15; //extern const static void bool in var for MAP
 
 char disp_map [MAP_HEIGHT][MAP_WIDTH];
 
-std::vector<Object> list_of_objects;
+std::vector<Object*> list_of_objects;
 
-std::string mainOutput;
+std::string main_output = "";
 
-void introduce_object(Object o) {
-	list_of_objects.push_back(o);
+void introduce_object(Object* o) {
+	list_of_objects.push_back(o); //insert the index of o
 }
 
 std::vector<Object*> getObjectsAtXY(int x, int y) {
 	std::vector<Object*> _objects;
 	for (int m = 0; m < (short) list_of_objects.size(); m++) {
-		if (list_of_objects[m].x == x && list_of_objects[m].y == y) { //if it matches at that xy-pos
-			_objects.push_back(&list_of_objects[m]); //add the address of that match
+		if (list_of_objects[m]->x == x && list_of_objects[m]->y == y) { //if it matches at that xy-pos
+			_objects.push_back(list_of_objects[m]); //add the address of that match
 		}
 	}
 	return _objects;
@@ -54,27 +54,28 @@ void refresh_map() { //Go through every X and Y position in the map, and get eac
 
 void display_map() {
 	for (int i = 0; i < MAP_HEIGHT; i++) {std::cout << "\n";} //give space for next frame
-	std::cout << mainOutput << "\n"; //show the output
+	std::cout << main_output; //show the output, no \n
 	for (int y = 0; y < MAP_HEIGHT; y++) {
 		for (int x = 0; x < MAP_WIDTH; x++) {
 			std::cout << disp_map[y][x];
 			if (x == MAP_WIDTH - 1) {std::cout << "\n";} //if its the last character in the line, newline
 		}
 	}
-	mainOutput = ""; //clear the output
+	main_output = ""; //clear the output
 }
 
 
 int main() {
 	std::cout << "Starting...\n";
-	//Set up the map
 
+	//Set up the map
 	Player player (5, 8); //set up the player
-	introduce_object(player);
+	introduce_object(&player);
 
 	for (int y = 0; y < MAP_HEIGHT; y++) {
 		for (int x = 0; x < MAP_WIDTH; x++) {
-			introduce_object(Object(1, x, y)); //fills the room with floor
+			Object* floor = new Object(1, x, y);
+			introduce_object(floor); //fill the map with floor
 		}
 	}
 
@@ -85,7 +86,7 @@ int main() {
 		refresh_map();
 		display_map();
 
-		std::cout << "Player pos: " << player.x << ", " << player.y << " | " << player.disp << "\n";
+		std::cout << "Player pos: " << player.x << ", " << player.y << "\n";
 
 		//Input
 		input = ' ';
